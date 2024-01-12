d9b1fe5bfe270a6f48be3dd481407f4663894926
Jesse Nay <jessenay26@gmail.com>
2024-01-12 15:54:08 -0700

    added more code to server.js but I realized i'm using express and i should be using inquirer

--- server.js
+++ server.js
@@ -8,14 +8,34 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: process.env.USER_DB,
     password: process.env.PASSWORD_DB,
-    database: employees_db
-}, console.log('Connecte to the employees database'));
+    database: "employees_db"
+}, console.log('Connected to the employees database'));
 
 const app = express();
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get
+app.get('/api/departments', (req, res) => {
+    db.query("SELECT * FROM departments", (err, result) => {
+        if (err) {
+            res.json('{"message": "an error occurred"}').status(500);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+app.post('/api/departments', (req, res) => {
+    const { id, department_name } = req.body;
+
+    db.query("INSERT INTO departments (id, department_name) VALUES (?, ?)", [id, department_name], (err, result) => {
+        if (err) {
+            res.json('{"message": "an error occurred"}').status(500);
+        } else {
+            res.json(result);
+        }
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
